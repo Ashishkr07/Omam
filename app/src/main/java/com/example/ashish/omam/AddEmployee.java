3296c1551b5613c24ae4e70b01df42ea17dbfb00
Ashishkr07 <ksndelash@gmail.com>
2018-06-18 14:57:13 +0530

    second commit

--- app/src/main/java/com/example/ashish/omam/AddEmployee.java
+++ app/src/main/java/com/example/ashish/omam/AddEmployee.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by ashish on 16/6/18.
@@ -12,15 +15,75 @@ import android.widget.EditText;
 
 public class AddEmployee extends AppCompatActivity {
 
-    EditText e1,e2,e3;
+    Spinner s1,s2;
+    String department = " ";
+    String branch = " ";
+    EditText e1;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addemployee);
         e1 = (EditText) findViewById(R.id.ed1);
-        e2 = (EditText) findViewById(R.id.ed2);
-        e3 = (EditText) findViewById(R.id.ed3);
+        s1 = (Spinner) findViewById(R.id.spinr1);
+        s2 = (Spinner) findViewById(R.id.spinr2);
 
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(AddEmployee.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.position));
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(arrayAdapter1);
+        s1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                       department  = "Consultant";
+                        break;
+                    case 1:
+                        department = "HR";
+                        break;
+                    case 2:
+                        department = "Administration";
+                        break;
+                    case 3:
+                        department = "Counselor";
+                        break;
+                    case 4:
+                        department = "Marketing";
+                        break;
+                    case 5:
+                        department = "Faculty";
+                        break;
+                    case 6:
+                        department = "Center Head";
+                        break;
+                    case 7:
+                        department = "Business";
+                        break;
+                    case 8:
+                        department = "Telle Caller";
+                        break;
+                    case 9:
+                        department = "Other";
+                        break;
+                }
+            }
+
+        });
+
+
+
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(AddEmployee.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.branch));
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(arrayAdapter2);
+        s2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i ==0){
+                    branch = "DWARKA";
+                }else{
+                    branch = "RAMA VIHAR";
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/ashish/omam/RemoveEmployee.java
+++ app/src/main/java/com/example/ashish/omam/RemoveEmployee.java
@@ -3,7 +3,9 @@ package com.example.ashish.omam;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by ashish on 16/6/18.
@@ -11,14 +13,25 @@ import android.widget.EditText;
 
 public class RemoveEmployee extends AppCompatActivity{
 
-    EditText editText1,editText2,editText3;
+    EditText editText1;
+    Spinner sp1,sp2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.removeemployee);
-        editText1 = (EditText) findViewById(R.id.edt1);
-        editText2 = (EditText) findViewById(R.id.edt2);
-        editText3 = (EditText) findViewById(R.id.edt3);
+        editText1 = (EditText) findViewById(R.id.edt11);
+        sp1 = (Spinner) findViewById(R.id.spinr11);
+        sp2 = (Spinner) findViewById(R.id.spinr21);
+
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(RemoveEmployee.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.position));
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp1.setAdapter(arrayAdapter1);
+
+
+
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(RemoveEmployee.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.branch));
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp2.setAdapter(arrayAdapter2);
 
     }
 }
